df4cccff007bcd622cf3472010b2d83284604aea
JahManChild <oldmanchild@outlook.com>
2022-05-17 14:00:21 +0000

    Quick One

--- helloworld.cpp
+++ helloworld.cpp
@@ -2,5 +2,6 @@
 
 int main()
 {
-    std::cout << "Hello C++ World. First Commit via Command Line" << std::endl; 
+    std::cout << "Hello C++ World. First Commit via Command Line" << std::endl;
+    std::cout << "Hello C++ World. About to Push" << std::endl;
 }
