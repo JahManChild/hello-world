ab8650089b7c8425ce21a66889a972ed36788e14
JahManChild <oldmanchild@outlook.com>
2022-05-17 13:58:48 +0000

    FirstTestCommit

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello C++ World" << std::endl; 
+    std::cout << "Hello C++ World. First Commit via Command Line" << std::endl; 
 }
-=======
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello World" << std::endl; 
-}
->>>>>>> cf50875a2476cea1d182a3bd7b9da768fff63823
